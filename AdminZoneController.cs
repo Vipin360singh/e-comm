8cb923904089adca0943d824e194ae2ad6423049
Vipin Singh <93461500+Vipin360singh@users.noreply.github.com>
2023-08-17 21:41:41 +0530

    Add files via upload

---
+++ AdminZoneController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using e_comm.Content.Models;
+
+namespace e_comm.Controllers
+{
+    public class AdminZoneController : Controller
+    {
+        //
+        // GET: /AdminZone/
+        demo_contectEntities db = new demo_contectEntities();
+        public ActionResult Index()
+        {
+            if (Session["aid"] != null)
+            {
+
+            }
+            else
+            {
+                Response.Write("<script>alert('LogIn first then go to nextZone.');window.location.href='/Home/LogIn'</script>");
+            }
+
+            return View();
+
+        }
+        public ActionResult RegistrationMGMT()
+        {
+            if (Session["aid"] != null)
+            {
+
+            }
+            else
+            {
+                Response.Write("<script>alert('LogIn first then go to nextZone.');window.location.href='/Home/LogIn'</script>");
+            }
+            List<TBL_Reg> Lst = null;
+            Lst = db.TBL_Reg.ToList();
+            return View(Lst);
+        }
+        public void Delete()
+        {
+            try
+            {
+                string m = Request.QueryString["m"];
+                TBL_Reg tbl1 = db.TBL_Reg.SingleOrDefault(x => x.Email == m);
+                db.TBL_Reg.Remove(tbl1);
+                db.SaveChanges();
+                Response.Write("<script>alert('record DELETED successfully');window.location.href='/AdminZone/RegistrationMGMT'</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('record not deleted successfully');window.location.href='/AdminZone/RegistrationMGMT'</script>");
+            }
+        }
+        public void logout()
+        {
+            Session.Abandon();
+            Response.Write("<script>alert('logOut');window.location.href='/Home/LogIn'</script>");
+        }
+
+        //GEt action call
+        [HttpGet]
+        public ActionResult RecordUpdate()
+        {
+            string Email = Request.QueryString["u"];
+            TBL_Reg Reg = db.TBL_Reg.SingleOrDefault(x => x.Email == Email);
+            return View(Reg);
+        }
+        [HttpPost]
+        public void RecordUpdate(TBL_Reg reg, string Email)
+        {
+            TBL_Reg rg = db.TBL_Reg.SingleOrDefault(x => x.Email == Email);
+            try
+            {
+                HttpPostedFileBase file = Request.Files["Profile"];
+                reg.Profile = file.FileName;
+                if (file.FileName != "")
+                {
+
+                    rg.Password = reg.Password;
+                    rg.Address = reg.Address;
+                    rg.city = reg.city;
+                    rg.State = reg.State;
+                    rg.Pin = reg.Pin;
+                    rg.Profile = reg.Profile;
+                    db.SaveChanges();
+                    file.SaveAs(Server.MapPath("~/Content/Profile/" + file.FileName));
+                    Response.Write("<script>alert('recorder updated successfully');window.location.href='/AdminZone/RegistrationMGMT'</script>");
+                }
+                else
+                {
+                    TBL_Reg rt = db.TBL_Reg.SingleOrDefault(x => x.Email == Email);
+                    rt.Password = reg.Password;
+                    rt.Address = reg.Address;
+                    rt.city = reg.city;
+                    rt.State = reg.State;
+                    rt.Pin = reg.Pin;
+                    rt.Profile = reg.Profile;
+                    db.SaveChanges();
+                    file.SaveAs(Server.MapPath("~/Content/Profile/" + file.FileName));
+                    Response.Write("<script>alert('recorder not updated succesfully');window.location.href='/AdminZone/RegistrationMGMT'</script>");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('recorder not updated');window.location.href='/AdminZone/RegistrationMGMT'</script>");
+            }
+        }
+
+
+
+        public ActionResult ContectMGMT()
+        {
+            List<TBL_Contact> con = null;
+            con = db.TBL_Contact.ToList();
+            return View(con);
+        }
+        public ActionResult ReturnMGMT()
+        {
+            List<TBL_ReturnOrder> RET = null;
+            RET = db.TBL_ReturnOrder.ToList();
+            return View(RET);
+        }
+        public ActionResult ComplainMGMT()
+        {
+            List<TBL_ComplainOrder> Com = null;
+            Com = db.TBL_ComplainOrder.ToList();
+            return View(Com);
+        }
+        public ActionResult CancleMGMT()
+        {
+            List<TBL_Cancle> Can = null;
+            Can = db.TBL_Cancle.ToList();
+            return View(Can);
+        }
+        public ActionResult ChangePassword()
+        {                           
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ChangePassword(string oldpassword, string newpassword, string confirmpassword)
+        {
+            if (newpassword == confirmpassword)
+            {
+                
+                string userid = Session["aid"].ToString();
+                TBL_login lg = db.TBL_login.Where(x => x.password == oldpassword && x.name == userid).SingleOrDefault();
+                lg.password = newpassword;
+                db.SaveChanges();
+                Response.Write("<script>alert('Your Password changed ');window.location.href='../Home/LogIn'</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('Plz confirm password')</script>");
+            }
+            return View();
+        }
+       
+
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using e_comm.Content.Models;
+
+namespace e_comm.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        demo_contectEntities db = new demo_contectEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //get action view for Registration
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        // use for save record code in reg
+        [HttpPost]
+        public ActionResult Registration(TBL_Reg REG,string hdn1,string ct1)
+        {
+            try
+            {
+                if (hdn1 == ct1)
+                {
+                    //wirte of profile
+                    HttpPostedFileBase file = Request.Files["Profile"];
+                    REG.Profile = file.FileName;
+                    file.SaveAs(Server.MapPath("~/Content/Profile/" + file.FileName));
+                    //get data & time
+                    REG.RegDate = DateTime.Now;
+                    //ENd data and tiem
+
+                    db.TBL_Reg.Add(REG);
+                    db.SaveChanges();
+                    Response.Write("<script>alert('Registered Successfully')</script>");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Some Problem in Registration')</script>");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Not Register')</script>");
+            }
+            return View();
+        }
+        //get action view for login
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+        // use for save record code in login
+        [HttpPost]
+        public ActionResult LogIn(TBL_login lg)
+        {
+            try
+            {
+            TBL_login t1 = db.TBL_login.Where(X => X.name == lg.name && X.password == lg.password).SingleOrDefault();
+            if (t1 != null)
+            {
+                Session["aid"] = t1.name;
+                Response.Write("<script>alert('Welcome to AdminZone');window.location.href='/AdminZone/Index'</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('Invailed Userid or Password')</script>");
+            }
+                
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Invailed Userid or Password')</script>");
+            }
+            return View();
+        }
+        
+        //get action view for contect
+        public ActionResult Contect()
+        {
+            return View();
+        }
+        // use for save record code in contect
+        [HttpPost]
+        public ActionResult Contect(TBL_Contact Con)
+        {
+            try
+            {
+                
+                db.TBL_Contact.Add(Con); 
+                db.SaveChanges();
+                Response.Write("<script>alert('Record Save Successfully')</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Record Not Save ')</script>");
+            }
+            return View();
+        }
+        public ActionResult Imagegallery()
+        {
+            return View();
+        }
+        public ActionResult Notification()
+        {
+            return View();
+        }
+        public ActionResult MyOrder()
+        {
+            return View();
+        }
+        public ActionResult OrderCancellation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult OrderCancellation(TBL_Cancle Can)
+        {
+            try
+            {
+                db.TBL_Cancle.Add(Can);
+                db.SaveChanges();
+                Response.Write("<script>alert('Your product cancellation Successfully')</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Your canclation rejected try again')</script>");
+            }
+            return View();
+        }
+        //get action view for Return Order
+        public ActionResult ReturnOrder()
+        {
+            return View();
+        }
+        //use for save record code in return Order
+        [HttpPost]
+        public ActionResult ReturnOrder(TBL_ReturnOrder RET)
+        {
+            try
+            {
+                //get data & time
+                //RET.RegDate = DateTime.Now;
+                db.TBL_ReturnOrder.Add(RET);
+                db.SaveChanges();
+                Response.Write("<script>alert('request save Successfully we will return your order soon')</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Your return request Not Save ')</script>");
+            }
+            return View();
+        }
+        //get action view for Complaint
+        public ActionResult OrderComplaint()
+        {
+            return View();
+        }
+        // use for save record code in complaint
+        [HttpPost]
+        public ActionResult OrderComplaint(TBL_ComplainOrder Com)
+        {
+            try
+            {
+                db.TBL_ComplainOrder.Add(Com);
+                db.SaveChanges();
+                Response.Write("<script>alert('Your Complaint Save Successfully')</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Your Complaint Not Save ')</script>");
+            }
+            return View();
+        }
+    }
+}
